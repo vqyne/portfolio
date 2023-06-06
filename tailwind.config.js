9c7b180fe5821e9b5e09abbc5adcdb519171994c
Loueï Bekhedda <bekheddalouei@gmail.com>
2023-06-06 10:25:40 +0200

    "refactor, new planet added"

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#050816",
+        primary: "#x",
+        //primary: "#050816",
         secondary: "#aaa6c3",
         tertiary: "#151030",
         "black-100": "#100d25",
